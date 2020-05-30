efa27a7e6115caf1582246cfc97471687eb02a2c
RJPearson94 <robpearson@pearson.co.uk>
2020-05-30 17:53:31 +0100

    feat: add data source to lookup studio flow

---
+++ twilio/internal/services/studio/data_source_studio_flow.go
@@ -0,0 +1,96 @@
+package studio
+
+import (
+	"fmt"
+
+	"github.com/RJPearson94/terraform-provider-twilio/twilio/common"
+	"github.com/RJPearson94/twilio-sdk-go/utils"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceStudioFlow() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceStudioFlowRead,
+		Schema: map[string]*schema.Schema{
+			"sid": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"account_sid": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"friendly_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"definition": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"commit_message": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"revision": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"valid": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"date_created": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"date_updated": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"webhook_url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceStudioFlowRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*common.TwilioClient).Studio
+
+	sid := d.Get("sid").(string)
+	getResponse, err := client.Flow(sid).Get()
+	if err != nil {
+		if _, ok := err.(*utils.TwilioError); ok {
+			if err.(*utils.TwilioError).IsNotFoundError() {
+				return fmt.Errorf("[ERROR] Studio flow with sid (%s) was not found", sid)
+			}
+		}
+		return fmt.Errorf("[ERROR] Failed to read studio flow: %s", err)
+	}
+
+	d.SetId(getResponse.Sid)
+	d.Set("sid", getResponse.Sid)
+	d.Set("account_sid", getResponse.AccountSid)
+	d.Set("friendly_name", getResponse.FriendlyName)
+	d.Set("definition", getResponse.Definition)
+	d.Set("status", getResponse.Status)
+	d.Set("revision", getResponse.Revision)
+	d.Set("commit_message", getResponse.CommitMessage)
+	d.Set("valid", getResponse.Valid)
+	d.Set("date_created", getResponse.DateCreated)
+	d.Set("date_updated", getResponse.DateUpdated)
+	d.Set("url", getResponse.URL)
+	d.Set("webhook_url", getResponse.WebhookURL)
+
+	return nil
+}
