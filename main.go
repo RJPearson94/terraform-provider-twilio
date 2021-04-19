0a426bf2f38140d89b55d9fe7998c9b8b80f866c
RJPearson94 <RJPearson94@users.noreply.github.com>
2021-04-19 20:06:01 +0100

    Revert "chore: add the ability to run the plugin mode"
    This reverts commit fc364c598b8e34ca242dd463a32b2725157d6a68.

--- main.go
+++ main.go
@@ -1,31 +1,12 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"log"
-
 	"github.com/RJPearson94/terraform-provider-twilio/twilio"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	var debugMode bool
-
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-
-	opts := &plugin.ServeOpts{
+	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: twilio.Provider,
-	}
-
-	if debugMode {
-		// For information on debugging the provider see, https://www.terraform.io/docs/extend/debugging.html
-		if err := plugin.Debug(context.Background(), "registry.terraform.io/RJPearson94/terraform-provider-twilio", opts); err != nil {
-			log.Fatal(err.Error())
-		}
-		return
-	}
-
-	plugin.Serve(opts)
+	})
 }
